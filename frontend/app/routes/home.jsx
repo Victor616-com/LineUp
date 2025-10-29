8b7afa961659a4bcdba728e7f614d8879e58a2e7
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-10-29 21:54:03 +0100

    Added email Signup and Login

--- frontend/app/routes/home.jsx
+++ frontend/app/routes/home.jsx
@@ -1,3 +1,50 @@
+"use client";
+import { useNavigate } from "react-router";
+import { UserAuth } from "../context/AuthContext";
+import PrivateRoute from "../components/PrivateRoute";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+
 export default function Home() {
-  return <main className="chat-container">Hello</main>;
+  const { session, signOut } = UserAuth();
+  const [profile, setProfile] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Sign out function
+  const handleSignOut = async (e) => {
+    e.preventDefault();
+    try {
+      await signOut();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    const loadProfile = async () => {
+      if (!session) return;
+      const { data } = await supabase
+        .from("profiles")
+        .select("username")
+        .eq("id", session.user.id)
+        .single();
+      setProfile(data);
+      console.log("Loaded profile:", data);
+    };
+    loadProfile();
+  }, [session]);
+
+  return (
+    <PrivateRoute>
+      <main className="font-extrabold text-m">
+        <h1>Home</h1>
+        <h2>Welcome, {profile?.username} 🎉</h2>
+        <p onClick={handleSignOut} className="text-blue-600 cursor-pointer">
+          Sign out
+        </p>
+      </main>
+    </PrivateRoute>
+  );
 }
