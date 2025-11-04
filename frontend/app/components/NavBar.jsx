5869e6cd31200fe45fa45e54432da1cd083f7479
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-11-04 17:05:33 +0100

    Made some comments in NavBar component for clarity

--- frontend/app/components/NavBar.jsx
+++ frontend/app/components/NavBar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+//The app is locked for only for users that are signed in so make an account before you start coding
+
+//I made all the routes that we will be using (at least necessary for navbar)
+//I made the navbar appear only after onboarding and login and have position fixed on top of the content.
+//You just have to make it
+//For styling you can check the app.css file to see what I predefined. You can predefine more if you need to.
+//I made some padding, corner radius and text styles. If you want to make text bold in tailwind you write font-bold
+//Tell chat that we are using tailwind but you can allways choose to use normal css.
+//In the app.css there is a "/* Regular css classes here */" and you can put regular classes there"
+//Good luck 🤞😉
+
 function NavBar() {
   return <div className="fixed bottom-0">NavBar</div>;
 }
