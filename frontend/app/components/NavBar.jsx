fec8afdb830b3ee88ef2d3cdc3b73fc0a779f916
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-11-04 16:53:32 +0100

    feat: Add new routes and components for Chats, Collabs, Services, and Create pages

---
+++ frontend/app/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NavBar() {
+  return <div className="fixed bottom-0">NavBar</div>;
+}
+
+export default NavBar;
--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -8,5 +8,9 @@ export default [
     route("signup", "routes/SignUp.jsx"),
     route("profile/:id", "routes/ProfileView.jsx"),
     route("profile/:id/edit", "routes/EditProfileView.jsx"),
+    route("collabs", "routes/CollabsPage.jsx"),
+    route("services", "routes/ServicesPage.jsx"),
+    route("chats", "routes/ChatsPage.jsx"),
+    route("create", "routes/CreatePage.jsx"),
   ]),
 ];
